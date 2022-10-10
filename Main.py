a543f2003f168cea2011789b483f2ed1ed9d95ad
URK21CS3003-KEFFALINA-S <109952094+URK21CS3003-KEFFALINA-S@users.noreply.github.com>
2022-10-10 16:36:58 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,27 +1,63 @@
 from typing import List
 
-def partition(array, low, high):
-    pivot = array[high]
-    i = low - 1
-    for j in range(low, high):
-        if array[j] <= pivot:
-            i = i + 1
-            (array[i], array[j]) = (array[j], array[i])
-    (array[i + 1], array[high]) = (array[high], array[i + 1])
-    return i + 1
-
-def quick_sort(array, low, high):
-    if low < high:
-        pi = partition(array, low, high)
-        quick_sort(array, low, pi - 1)
-        quick_sort(array, pi + 1, high)
-    return array
+def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
  
-input_data = input()
-data = []
-for item in input_data.split(', '):
-  if item.isnumeric():
-    data.append(int(item))
-  elif item.lstrip("-").isnumeric():
-    data.append(int(item))
-print(quick_sort(data, 0, len(data)-1))
+  x = nums1[0:m]
+  y = nums2[0:n]
+  nums1=x+y
+  nums1 = merge_sort(nums1)
+  return nums1
+  
+def merge_sort(myList) -> None:
+  if len(myList) > 1:
+        mid = len(myList) // 2
+        left = myList[:mid]
+        right = myList[mid:]
+
+        # Recursive call on each half
+        merge_sort(left)
+        merge_sort(right)
+
+        # Two iterators for traversing the two halves
+        i = 0
+        j = 0
+        
+        # Iterator for the main list
+        k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+              # The value from the left half has been used
+              myList[k] = left[i]
+              # Move the iterator forward
+              i += 1
+            else:
+                myList[k] = right[j]
+                j += 1
+            # Move to the next slot
+            k += 1
+
+        # For all the remaining values
+        while i < len(left):
+            myList[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            myList[k]=right[j]
+            j += 1
+            k += 1
+  return myList
+
+
+# Do not change the following code
+nums1 = []
+nums2 = []
+for item in input().split(', '):
+  nums1.append(int(item))
+for item in input().split(', '):
+  nums2.append(int(item))
+m = int(input())
+n = int(input())
+
+print(merge(nums1, m, nums2, n))
